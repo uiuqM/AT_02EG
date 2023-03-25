69ca8151fe0f8ed818644c6b0394ad9689743cb0
uiuqM <92444032+uiuqM@users.noreply.github.com>
2023-03-25 16:55:04 -0300

    adding nota model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class nota(models.Model):
+    titulo = models.CharField(max_length=100)
+    conteudo = models.TextField()
+    data = models.DateTimeField('data da nota')
