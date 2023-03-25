dd2674b2c09cb6bee98f17a2c0e6ab4bf5e51f62
uiuqM <92444032+uiuqM@users.noreply.github.com>
2023-03-25 15:29:53 -0300

    add view path polls

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("ótimo, esta é a página index de polls!")
