327c33d3e8ded4819f3b246fa27d993d7103419d
beneX90 <benedito1502@gmail.com>
2023-03-28 21:59:17 -0300

    antes do fim

--- app_notas/urls.py
+++ app_notas/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('usuarios/', views.usuarios, name='listagem_usuarios'),
+    path('index', views.index, name='index'),
     path('<int:nota_id>', views.detail, name='detail'),
     path('delete_nota/<int:id>', views.delete_nota, name="delete_nota"),
     path('mod_nota/<int:id>', views.mod_nota, name="mod_nota"),
